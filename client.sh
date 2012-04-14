f05d1fdd93e2c873a933d2e56aff599cfdf57e76
Anders Papitto <anderspapitto@gmail.com>
2012-04-14 10:43:24 -0700

    Structure of redirect-daemon.sh and client.sh finished.

--- client.sh
+++ client.sh
@@ -2,10 +2,8 @@
 
 ip=localhost
 port1=10001
-port2=10002
-
-nc $ip $port1
+port2=`nc $ip $port1`
 wait 4
-#nc.traditional $ip $port2 &
+nc $ip $port2 &
 echo type "'help'" to view your options
 fg
--- redirect-daemon.sh
+++ redirect-daemon.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
-mkdir ~/aber
-mkdir ~/aber/musicfiles
-nc.traditional -l -p 10001 -e $0 &
-nc.traditional -l -p 10002 | ./execute-commands.sh
-./execute-commands.sh
+
+
+if [ ! $DECALPORT ] ; then
+    echo -n $DECALPORT
+fi
+
+kill `lsof -i :10001 | tail -n +2 | sed -e 's,[ \t][ \t]*, ,g' | cut -f2 -d' '` # clears junk on port 10001
+nc.traditional -l -p 10001 -e $0 & #wait for more users and recurse when they come
+
+if [ ! $DECAL ] ; then # first time script is executed in a session
+    export DECAL=true # makes the above test work
+    export DECALPORT=10002 # further connections will start here
+    if [ ! -d ~/aber ] ; then # if our directory is not there, make it
+	mkdir ~/aber
+	mkdir ~/aber/musicfiles
+    fi
+    ./execute-commands.sh # allow the server-user to enter commands as well
+else
+    kill `lsof -i :$DECALPORT | tail -n +2 | sed -e 's,[ \t][ \t]*, ,g' | cut -f2 -d' '` # clears junk on DECALPORT
+    nc -l -p $DECALPORT | ./execute-commands.sh # space for one user connection
+    export DECALPORT=$(expr $DECALPORT + 1) # further users will go on higher ports
+fi
