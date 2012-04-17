7496872b4b02fec9d8d5f3c88a38b8c16d32ee18
Anders Papitto <anderspapitto@gmail.com>
2012-04-17 15:46:52 -0700

    cases added in execute-commands.sh

--- execute-commands.sh
+++ execute-commands.sh
@@ -4,6 +4,29 @@ echo "enter command:"
 
 while read command
 do
+	case $command in
+	play ) 
+	echo "play"
+	;;
+	stop ) 
+	echo "stop"
+	;;
+	playlist ) 
+	echo "playlist"
+	;;
+	remove[:space:][0-9][0-9] ) 
+	echo "remove" # do something with command
+	;;
+	fromTo[:space:][0-9][0-9][:space:][0-9][0-9] )
+	echo "fromTo"
+	;;
+	exit)
+	break
+	;;
+	*)
+	;;	
+	esac
+
     echo "enter command:"
     echo $command HI
 done
