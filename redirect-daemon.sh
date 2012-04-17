05b6a4c87b6498c068fdc38c7dc6f37c146e702e
Anders Papitto <anderspapitto@gmail.com>
2012-04-16 18:59:31 -0700

    two-way communication works

--- redirect-daemon.sh
+++ redirect-daemon.sh
@@ -9,7 +9,7 @@ if [ ! $DECAL ] ; then # first time script is executed in a session
     fi
 fi
 kill `lsof -i :$DECALPORT | tail -n +2 | sed -e 's,[ \t][ \t]*, ,g' | cut -f2 -d' '` 2>/dev/null # clears junk on DECALPORT
-nc -d -l $DECALPORT | ./execute-commands.sh & # space for one user connection
+\rm ~/aber/pipe 2>/dev/null; mknod ~/aber/pipe p && nc -l $DECALPORT 0<~/aber/pipe | ./execute-commands.sh 1>~/aber/pipe & # space for one user connection
 kill `lsof -i :10001 | tail -n +2 | sed -e 's,[ \t][ \t]*, ,g' | cut -f2 -d' '` 2>/dev/null # clears junk on port 10001
 clientresp=`echo $DECALPORT | nc -l 10001` #wait for more users
 export DECALPORT=$(expr $DECALPORT + 1) # further users will go on higher ports
